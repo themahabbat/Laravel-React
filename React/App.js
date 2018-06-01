8b12d6af069d6b27da0abb4fb934686403b45a05
Mahabbat Mammadov <mahabbatmusic@gmail.com>
2018-06-02 02:53:43 +0400

    Create App.js

---
+++ React/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+
+class Main extends React.Component {
+   render() {
+      return (
+         <BrowserRouter basename="/spotify">
+            <div>
+               <Route exact path="/about" component={() => (
+                  <span>ABOUT</span>
+               )} />
+               <Route exact path="/" component={() => (
+                  <span>HOME</span>
+               )} />
+               <hr/>
+               <Link to="/about">About</Link><br/>
+               <Link to="/">Home</Link>
+            </div>
+         </BrowserRouter>
+      )
+   }
+}
+
+ReactDOM.render(<Main />, document.querySelector('#app'))
